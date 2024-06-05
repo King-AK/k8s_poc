7964aef3ea742e3b0f04f9b32a02849b01caf1ea
king-ak <akdavey@email.wm.edu>
2024-06-05 16:23:45 -0400

    Update gradle, add test for Flink

---
+++ flink-ingestor/app/src/test/scala/com/kingak/flinkIngestor/Basics/TestFlinkWorks.scala
@@ -0,0 +1,42 @@
+package com.kingak.flinkIngestor.Basics
+
+import Utils.CollectSink
+import org.apache.flinkx.api._
+import org.apache.flinkx.api.serializers._
+import org.junit.runner.RunWith
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatestplus.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class TestFlinkWorks extends AnyFunSuite with BeforeAndAfterEach {
+
+  val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+  test("Flink works") {
+    assert(env != null)
+  }
+
+  test("Flink map works") {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+    // configure your test environment
+    env.setParallelism(2)
+
+    // values are collected in a static variable
+    CollectSink.values.clear()
+
+    env.fromElements(2)
+      .map(_ + 1)
+      .addSink(new CollectSink)
+
+    // execute
+    env.execute()
+
+    // verify
+    assert(CollectSink.values.get(0) == 3)
+
+
+  }
+
+}
---
+++ flink-ingestor/app/src/test/scala/com/kingak/flinkIngestor/Utils/CollectSink.scala
@@ -0,0 +1,20 @@
+package Utils
+
+import org.apache.flink.streaming.api.functions.sink.SinkFunction
+
+import java.util
+import java.util.Collections
+
+// create a testing sink
+class CollectSink extends SinkFunction[Int] {
+
+  override def invoke(value: Int, context: SinkFunction.Context): Unit = {
+    CollectSink.values.add(value)
+  }
+}
+
+object CollectSink {
+  // must be static
+  val values: util.List[Int] = Collections.synchronizedList(new util.ArrayList[Int]())
+}
+
